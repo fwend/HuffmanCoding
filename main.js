e3a97fc4365eec0f9ea775beb0263218dee9609e
Jos van Uden <josvanuden@gmail.com>
2019-03-10 13:53:34 +0100

    change input format

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
 import MinHeap from './minheap.js';
 
 class HuffmanNode { 
-    constructor(freq, char) {
-        this.freq = freq; 
+    constructor(char, freq) {
         this.char = char; 
+        this.freq = freq;         
     }
 }
 
-const huffmanEncode = (arr, freq) => {
-    const priorityQueue = new MinHeap(arr.length, (a, b) => a.freq < b.freq);
-       
+const huffmanEncode = (data) => {
+    const priorityQueue = new MinHeap(data.length, (a, b) => a.freq < b.freq);
+
     // build min heap
-    for (let i = 0; i < arr.length; i++) { 
-        const node = new HuffmanNode(freq[i], arr[i]); 
+    for (const entry of data) { 
+        const node = new HuffmanNode(entry[0], entry[1]); 
         priorityQueue.insertKey(node); 
     } 
-    
+
     // build huffman tree
     while (priorityQueue.size > 1) { 
 
         const x = priorityQueue.extractMin(); 
         const y = priorityQueue.extractMin(); 
         
-        const z = new HuffmanNode(x.freq + y.freq, null); 
+        const z = new HuffmanNode(null, x.freq + y.freq); 
         z.left = x;
         z.right = y;
         priorityQueue.insertKey(z);        
     } 
+    
     printCode(priorityQueue.getMin(), ""); 
 };
   
 const printCode = (root, s) => { 
     if (root.char) { 
-        console.log(root.char + ":" + s); 
+        console.log(root.char + " : " + s); 
     } else {
         printCode(root.left, s + "0"); 
         printCode(root.right, s + "1"); 
     }
 }; 
 
-huffmanEncode(['a', 'b', 'c', 'd', 'e', 'f'], [45, 13, 12, 16, 9, 5]);
+const table = [
+    ['a' , 45], ['b' , 13], ['c', 12], ['d' , 16], ['e' , 9], ['f' , 5]
+]
+
+huffmanEncode(table);
 
 
